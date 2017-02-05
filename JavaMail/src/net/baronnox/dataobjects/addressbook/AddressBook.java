659d08cf7bcee608aa70dd684c6ea355e40d5841
BaronNox <baron.von.stabossem@gmail.com>
2017-02-05 05:23:21 +0100

    +prepped for serialization

--- JavaMail/src/net/baronnox/dataobjects/addressbook/AddressBook.java
+++ JavaMail/src/net/baronnox/dataobjects/addressbook/AddressBook.java
@@ -1,9 +1,15 @@
 package net.baronnox.dataobjects.addressbook;
 
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
-public class AddressBook {
+public class AddressBook implements Serializable {
 	private List<Contact> contactList;
+	
+	public AddressBook() {
+		this.contactList = new ArrayList<>();
+	}
 	
 	public void addContactToList(Contact contact) {
 		this.contactList.add(contact);
