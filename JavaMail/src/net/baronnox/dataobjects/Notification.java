72d986594530bddc4b73a5e7bb9f48d0344b0be2
BaronNox <baron.von.stabossem@gmail.com>
2017-02-06 06:59:12 +0100

    refactoring

--- JavaMail/src/net/baronnox/dataobjects/Notification.java
+++ JavaMail/src/net/baronnox/dataobjects/Notification.java
@@ -1,8 +1,8 @@
 package net.baronnox.dataobjects;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 import net.baronnox.dataobjects.addressbook.Contact;
 import net.baronnox.dataobjects.utility.NotificationDays;
@@ -10,22 +10,22 @@ import net.baronnox.dataobjects.utility.NotificationDays;
 public class Notification implements Serializable {
 	private static final long serialVersionUID = Long.parseUnsignedLong("1");
 	
-	private List<Contact> recipients;
+	private ArrayList<Contact> recipients;
 	private Date due;
 	private Date end;
-	private List<NotificationDays> dueDays;
+	private ArrayList<NotificationDays> dueDays;
 	
 	private String subject;
 	private String msg;
 	
-	public Notification(List<Contact> recipients, Date due, String subject, String msg) {
+	public Notification(ArrayList<Contact> recipients, Date due, String subject, String msg) {
 		this.recipients = recipients;
 		this.due = due;
 		this.subject = subject;
 		this.msg = msg;
 	}
 	
-	public List<Contact> getRecipients() {
+	public ArrayList<Contact> getRecipients() {
 		return recipients;
 	}
 	
