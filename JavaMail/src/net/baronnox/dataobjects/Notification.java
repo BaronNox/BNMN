157e76ad77245b53a01c718a8687c204678ca76b
BaronNox <baron.von.stabossem@gmail.com>
2017-02-06 05:52:03 +0100

    +notification subject and getter/setter

--- JavaMail/src/net/baronnox/dataobjects/Notification.java
+++ JavaMail/src/net/baronnox/dataobjects/Notification.java
@@ -8,10 +8,36 @@ import net.baronnox.dataobjects.addressbook.Contact;
 import net.baronnox.dataobjects.utility.NotificationDays;
 
 public class Notification implements Serializable {
+	private static final long serialVersionUID = Long.parseUnsignedLong("1");
+	
 	private List<Contact> recipients;
 	private Date due;
 	private Date end;
 	private List<NotificationDays> dueDays;
 	
+	private String subject;
 	private String msg;
+	
+	public Notification(List<Contact> recipients, Date due, String subject, String msg) {
+		this.recipients = recipients;
+		this.due = due;
+		this.subject = subject;
+		this.msg = msg;
+	}
+	
+	public List<Contact> getRecipients() {
+		return recipients;
+	}
+	
+	public String getMsg() {
+		return msg;
+	}
+	
+	public Date getDue() {
+		return due;
+	}
+	
+	public String getSubject() {
+		return subject;
+	}
 }
