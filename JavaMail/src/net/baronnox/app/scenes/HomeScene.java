20c3580df9cec36e9bb75936321ada93c4b9ac39
Jonas v. St <jonas.stowasser@stud.uni-bamberg.de>
2017-02-10 11:49:22 +0100

    Major refactoring
    created init-Methods for all UI Control groups.

--- JavaMail/src/net/baronnox/app/scenes/HomeScene.java
+++ JavaMail/src/net/baronnox/app/scenes/HomeScene.java
@@ -20,6 +20,7 @@ import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.SeparatorMenuItem;
 import javafx.scene.control.Tooltip;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
@@ -40,9 +41,19 @@ public class HomeScene {
 	private Scene scene;
 	private Account acc;
 	private ScrollPane scrollPane;
-	private ListView<Contact> listView;
 	
 	private Contact oldSelection = null;
+	
+	//UI Controls
+	private Button createContactBtn;
+//	private MenuBar menuBar;
+	private VBox vBox;
+	private VBox vBoxButtons;
+	private HBox hBoxButtons;
+	private GridPane gridPane;
+	private ListView<Contact> contactListView;
+//	private ListView<String> notifListView;
+	//END UI Controls
 	
 	public HomeScene(Stage primaryStage, Account acc) {
 		this.primaryStage = primaryStage;
@@ -82,97 +93,119 @@ public class HomeScene {
 	}
 
 	private void initScene() {
+		vBox = new VBox(0); 		 //Main Scene Layout
+		scrollPane.setContent(vBox);
+		initMenuBar();
+		
 		primaryStage.setResizable(true);
 		
-		VBox vBox = new VBox(0);
-		scrollPane.setContent(vBox);
+		gridPane = initGridPane();	 //Rest of Window Layout
+		vBox.getChildren().add(gridPane);
 		
+		contactListView = initContactListView();
+		initNotifListView();
+		
+		hBoxButtons = new HBox(3);	//was cBtnBox
+		hBoxButtons.setAlignment(Pos.CENTER);
+		gridPane.add(hBoxButtons, 0, 1);
+		
+		vBoxButtons = new VBox();	//was buttonBox
+		gridPane.add(vBoxButtons, 1, 0);
+
+		initButtons();
+	}
+	
+	private MenuBar initMenuBar() {
 		MenuBar menuBar = new MenuBar();
-		menuBar.prefWidthProperty().bind(primaryStage.widthProperty());
+		
+		menuBar.setPrefWidth(WIDTH - 2);
 		final Menu file = new Menu("File");
 		final Menu help = new Menu("Help");
-		MenuItem createContact = new Menu("Create New Contact");
-		file.getItems().add(createContact);
-		menuBar.getMenus().addAll(file, help);
+		MenuItem createContactMItem = new MenuItem("Create New Contact");
+		createContactMItem.setOnAction(e -> {
+			createContactBtn.fire();
+		});
+		MenuItem exitMItem = new MenuItem("Exit");
+		MenuItem manualMItem = new MenuItem("Manual");
+		MenuItem aboutMItem = new MenuItem("About");
+		SeparatorMenuItem separatorMenuItem = new SeparatorMenuItem();
+		SeparatorMenuItem separatorMenuItem2 = new SeparatorMenuItem();
 		
+		file.getItems().addAll(createContactMItem, separatorMenuItem, exitMItem);
+		help.getItems().addAll(manualMItem, separatorMenuItem2, aboutMItem);
+		menuBar.getMenus().addAll(file, help);
 		vBox.getChildren().add(menuBar);
 		
+		return menuBar;
+	}
+	
+	private GridPane initGridPane() {
 		GridPane gridPane = new GridPane();
 		gridPane.setAlignment(Pos.TOP_LEFT);
 		gridPane.setPrefWidth(WIDTH - 2);
-		gridPane.setGridLinesVisible(true);
+		gridPane.setGridLinesVisible(true); //DEBUG
 		
-//		Pane innerPane = new Pane();
-//		innerPane.setPrefHeight(HEIGHT / 2);
-//		innerPane.setMinHeight(HEIGHT / 2);
-//		innerPane.setMaxHeight(HEIGHT / 2);
+		return gridPane;
+	}
+	
+	private ListView<Contact> initContactListView() {
+		ListView<Contact> contactListView = new ListView<>();
 		
-		listView = new ListView<>();
-		listView.setItems(FXCollections.observableArrayList(acc.getAddressBook().getContactList()));
-//		listView.setPrefHeight(innerPane.getPrefHeight());
-		listView.setPrefHeight(150);
-//		innerPane.getChildren().add(listView);
+		contactListView.setItems(FXCollections.observableArrayList(acc.getAddressBook().getContactList()));
+		contactListView.setPrefHeight(150);
+		gridPane.add(contactListView, 0, 0);
 		
-//		gridPane.add(innerPane, 0, 0);
-		gridPane.add(listView, 0, 0);
+		return contactListView;
+	}
+	
+	private ListView<String> initNotifListView() {
+		ListView<String> notifListView = new ListView<>();
 		
-//		Pane contentPane = new Pane();
-//		contentPane.setPrefHeight(HEIGHT / 2);
-//		contentPane.setMinHeight(HEIGHT / 2);
-//		contentPane.setMaxHeight(HEIGHT / 2);
-		
-		ListView<String> notifList = new ListView<>();
-//		notifList.setPrefHeight(contentPane.getPrefHeight());
-		notifList.setPrefHeight(150);
-		listView.setOnMouseClicked(e -> {
-			Contact selected = listView.getSelectionModel().getSelectedItem();
+		notifListView.setPrefHeight(150);
+		contactListView.setOnMouseClicked(e -> {
+			Contact selected = contactListView.getSelectionModel().getSelectedItem();
 			if(selected != null && !selected.equals(oldSelection)) {
-				notifList.getItems().clear();
+				notifListView.getItems().clear();
 				for(Notification n : acc.getAddressBook().getNotificationByContact(selected)) {
-					notifList.getItems().add("Subject: " + n.getSubject());
-					notifList.getItems().add("Recipients: " + n.getRecipients());
-					notifList.getItems().add("Due: " + n.getDue());
-					notifList.getItems().add("Message: \n" + n.getMsg());
+					notifListView.getItems().add("Subject: " + n.getSubject());
+					notifListView.getItems().add("Recipients: " + n.getRecipients());
+					notifListView.getItems().add("Due: " + n.getDue());
+					notifListView.getItems().add("Message: \n" + n.getMsg());
 				}
 			}
 			oldSelection = selected;
 		});
+		gridPane.add(notifListView, 2, 0);
 		
-//		contentPane.getChildren().add(notifList);
-//		gridPane.add(contentPane, 2, 0);
-		gridPane.add(notifList, 2, 0);
-		
-		HBox cBtnBox = new HBox(3);
-		cBtnBox.setAlignment(Pos.CENTER);
-		
-		VBox buttonBox = new VBox();
+		return notifListView;
+	}
+	
+	private void initButtons() {
+		createContactBtn = new Button("Create new Contact");
+		Button delContactBtn = new Button("Remove Contact");
 		Button addContact = new Button("Add Contact");
+		Button saveBtn = new Button("Save");
+		
+		
 		addContact.setPrefWidth(125);
 		addContact.setTooltip(new Tooltip("Adds selected Contact to Notification"));
 		//TODO: Click logic
 		//END
+		gridPane.add(addContact, 1, 0);
+//		hBoxButtons.getChildren().add(addContact);
 		
-		buttonBox.getChildren().add(addContact);
-		gridPane.add(buttonBox, 1, 0);
+		delContactBtn.setTooltip(new Tooltip("Delete selected Contact from list."));
+		delContactBtn.setOnAction(e -> {
+			Contact selection = contactListView.getSelectionModel().getSelectedItem();
+			contactListView.getItems().remove(selection);
+		});
+		hBoxButtons.getChildren().add(delContactBtn);
 		
-		
-		
-		Button createContactBtn = new Button("Create new Contact");
 		createContactBtn.setOnAction(e -> {
 			new PUCreateContact(this);
 		});
-		cBtnBox.getChildren().add(createContactBtn);
+		hBoxButtons.getChildren().add(createContactBtn);
 		
-		Button delContactBtn = new Button("Remove Contact");
-		delContactBtn.setTooltip(new Tooltip("Delete selected Contact from list."));
-		delContactBtn.setOnAction(e -> {
-			Contact selection = listView.getSelectionModel().getSelectedItem();
-			listView.getItems().remove(selection);
-		});
-		cBtnBox.getChildren().add(delContactBtn);
-		gridPane.add(cBtnBox, 0, 1);
-		
-		Button saveBtn = new Button("Save");
 		saveBtn.setOnAction(e -> {
 			try {
 				File dataDir = new File(DATA_DIR);
@@ -187,13 +220,9 @@ public class HomeScene {
 				e1.printStackTrace();
 			}
 		});
-		gridPane.add(saveBtn, 0, 2);
-		
-		vBox.getChildren().add(gridPane);
-		
-		
+		gridPane.add(saveBtn, 0, 2); //TODO: Change location
 	}
-	
+
 	public Scene getScene() {
 		return this.scene;
 	}
@@ -207,6 +236,6 @@ public class HomeScene {
 	}
 	
 	public void updateContactListView() {
-		listView.setItems(FXCollections.observableArrayList(acc.getAddressBook().getContactList()));
+		contactListView.setItems(FXCollections.observableArrayList(acc.getAddressBook().getContactList()));
 	}
 }
